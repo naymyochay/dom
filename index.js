7bc2e7b885358b850b96d1a502fbe2b77a580c88
naymyochay <naymyoaung.atechnician@gmail.com>
2022-09-20 04:27:41 -0700

    firstCommit

---
+++ index.js
@@ -0,0 +1,3 @@
+let jsonString = '{"Teacher" : "Thida Soe", "Student" : "Nay Myo Aung", "Roll No" : "3GIT-14"}';
+let jsonNumber = {"Age" : 55, "Family Number" : 81, "Student Number" : 500};
+let jsonBoolean = {};
